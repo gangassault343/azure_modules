e5bdb0f5e6c8374f9c1374c31ab34d7b78ab3c99
ARUN M L <arunmlascl@gmail.com>
2026-04-22 11:49:22 +0530

    Update main.tf

--- environments/main.tf
+++ environments/main.tf
@@ -1,1 +1,19 @@
 
+resource "azurerm_resource_group" "rg" {
+  name     = "dev-rg"
+  location = "East US"
+}
+
+module "network" {
+  source    = "../../modules/network"
+  vnet_name = "dev-vnet"
+  location  = "East US"
+  rg_name   = azurerm_resource_group.rg.name
+}
+
+module "compute" {
+  source    = "../../modules/compute"
+  location  = "East US"
+  rg_name   = azurerm_resource_group.rg.name
+  subnet_id = module.network.subnet_id
+}
